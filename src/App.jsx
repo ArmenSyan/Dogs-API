34ceb5fc5b89abf5f7361ef1787a9f9192692113
ArmenSyan <armen.sahakyan.ait@gmail.com>
2024-12-10 18:29:36 +0400

    Del all another files and codes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState([])
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
